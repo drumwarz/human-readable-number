d414b1f6315dcf554c5035bf668268d4313ab50f
Ihar Sapeha <maddavoice@gmail.com>
2022-12-23 12:16:47 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,39 +1,69 @@
-module.exports = function toReadable (number) {
-	const numUnits = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-	const numDozens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
-	const numRound = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
-	const numStr = number.toString();
-	const unit = number % 10;
-	const hundred = number % 100;
- 
-	if (number < 10) {
-	  result = numUnits[number];
- 
-	} else if (number >= 10 && number < 20) {
-	  result = numDozens[numStr[1]];
- 
-	} else if (number >=20 && number < 100) {
-	  if (unit === 0) {
-		 result = numRound[numStr[0] - 2];
-	  } else {
-		 result = `${numRound[numStr[0] -2]} ${numUnits[unit]}`;
-	  }
- 
-	} else if (number >= 100 && number < 1000 && hundred === 0) {
-	  result = `${numUnits[numStr[0]]} hundred`;
- 
-	} else if (hundred < 10 && hundred >= 1) {
-	  result = `${numUnits[numStr[0]]} hundred ${numUnits[unit]}`;
- 
-	} else if (hundred < 20 && hundred >= 10) {
-	  result = `${numUnits[numStr[0]]} hundred ${numDozens[unit]}`;
-	
-	} else if (number < 1000) {
-	  if (unit === 0) {
-		 result = `${numUnits[numStr[0]]} hundred ${numRound[numStr[1] - 2]}`;
-	  } else {
-		 result = `${numUnits[numStr[0]]} hundred ${numRound[numStr[1] - 2]} ${numUnits[unit]}`;
-	  }
-	}
-	return result;
-}
+module.exports = function toReadable(number) {
+    let digit1 = [
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+    ];
+    let digit2 = [
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+    let digit3 = [
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+
+    let res = "";
+
+    let start = number.toString().slice(0, 1);
+    let center = number.toString().slice(1, 3);
+    let end = number.toString().slice(2, 4);
+
+    if (number === 0) {
+        res += "zero";
+    } else if (number < 10) {
+        res += digit1[number - 1];
+    } else if (number < 20) {
+        res += digit2[number - 10];
+    } else if (number < 100) {
+        res += `${digit3[+start - 2]} ${
+            +center !== 0 ? digit1[+center - 1] : ""
+        }`;
+    } else if (number < 1000 && +center == 0) {
+        res += `${digit1[+start - 1]} hundred`;
+    } else if (number < 1000 && +center < 10) {
+        res += `${digit1[+start - 1]} hundred ${digit1[+end - 1]}`;
+    } else if (number < 1000 && +center < 20) {
+        res += `${digit1[+start - 1]} hundred ${digit2[+center - 10]}`;
+    } else if (number < 1000 && +end === 0) {
+        res += `${digit1[+start - 1]} hundred ${
+            digit3[+center.slice(0, 1) - 2]
+        }`;
+    } else if (number < 1000 && +end !== 0) {
+        res += `${digit1[+start - 1]} hundred ${
+            digit3[+center.slice(0, 1) - 2]
+        } ${digit1[+end - 1]}`;
+    }
+
+    return res.trim();
+};
